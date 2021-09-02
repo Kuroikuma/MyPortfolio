90aa3ee343b9d78a653382863de8a6aa5e0bd835
kuroikuma <odiseoj676@gmail.com>
2021-09-02 17:41:21 -0600

    parte del intesction del header

--- src/app/page/App/App.js
+++ src/app/page/App/App.js
@@ -1,11 +1,29 @@
 import "./App.css";
+import React, { useState, useEffect, useRef } from "react";
 import Header from "../../Components/header";
 
 function App() {
+  const [colorHeader, setColorHeader] = useState("white");
+  const elementRef = useRef();
+  useEffect(() => {
+    const onChange = (entries) => {
+      const el = entries[0];
+      if (el.isIntersecting) {
+        setColorHeader("black");
+        console.log("huuh");
+      }
+    };
+    const observer = new IntersectionObserver(onChange, {
+      rootMargin: "0px",
+    });
+    observer.observe(elementRef.current);
+  });
   return (
-    <>
+    <div className="container">
       <Header />
-    </>
+      <div className="k">fhgfgrfg</div>
+      <div ref={elementRef}>trt</div>
+    </div>
   );
 }
 
